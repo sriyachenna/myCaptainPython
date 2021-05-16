62212ffa44e5c7b82901fc3ad5d33929ff47cc5a
sriyachenna <69467357+sriyachenna@users.noreply.github.com>
2021-05-16 17:45:06 +0530

    Create positiverange.py

---
+++ positiverange.py
@@ -0,0 +1,11 @@
+list1 = [12, -7, 5, 64, -14]
+list2=[12,14,-95,3]
+list3=[]
+for i in list1:
+    if i>0:
+        print(i,end=",")       
+print("\n")
+for i1 in list2:
+    if i1>0:
+        list3.append(i1)
+print(list3)
